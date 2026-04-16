1b6c1f5d81fb59c18d32d8ea558140e6db42da2f
Gustavo Almeida <gustavofederalgg@gmail.com>
2026-04-16 20:20:06 -0300

    refactor: Refatoração total do codgo melhorando sua estrutura  sem alteração nos testes

--- senhas.py
+++ senhas.py
@@ -1,16 +1,14 @@
 import re
 
-def validar_senha(senha):
-    if len(senha)< 8:
-        return False
-    if " " in senha:
-        return False
+def validar_senha(senha: str) -> bool:
     
-    tem_maiuscula = re.search(r'[A-Z]', senha)
-    tem_minuscula = re.search(r'[a-z]', senha)
-    tem_numero = re.search(r'[0-9]', senha)
-    tem_especial = re.search(r'[!@#$%^&*]', senha)
-    
-    if not (tem_numero and tem_especial and tem_maiuscula and tem_maiuscula):
-        return False
-    return True
+    regras =[
+
+        len(senha)< 8,
+        " " not in senha,
+        re.search(r'[A-Z]', senha),
+        re.search(r'[a-z]', senha),
+        re.search(r'[0-9]', senha),
+        re.search(r'[!@#$%^&*()_+={}\[\]|\\:;,.<>?/]', senha)
+    ]
+    return all(regras)
