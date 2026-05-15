f28bc4e02d08ffa30f3540c35b2aec66d97e7f3e
Almeida <107077336+Almeidoka@users.noreply.github.com>
2026-05-15 15:57:17 -0300

    test: deve rejeitar senhas sem nenhuma letra minuscula

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import pytest_
 def test_letra_maiuscula():
     assert validacao_senha("asdadasd09") is False
 
+def test_letra_minuscula():
+    assert validacao_senha("ASDASDA!1") is False
+
 def test_numero():
     assert validacao_senha("asdadaA!a") is False
 
 def test_minimo_caracteres():
     assert validacao_senha("123a") is False
+
