bb6d465dd9f8fc33ab1c5ce19c66ba6d94e1aac9
Almeida <107077336+Almeidoka@users.noreply.github.com>
2026-05-15 15:52:04 -0300

    test: deve rejeitar senhas sem uma letra maiuscula

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-import unittest
+import pytest_
 
-from senhas import validar_senha
-
-class TestSenha(unittest.TestCase):
-
-    def test_senha_curta(self):
-        self.assertFalse(validar_senha("Ab1!"))
-
-    def test_senha_sem_maiuscula(self):
-        self.assertFalse(validar_senha("senha123!"))
-
-    def test_senha_sem_minuscula(self):
-        self.assertFalse(validar_senha("SENHA!123"))
-
-    def test_senha_sem_numero(self):
-        self.assertFalse(validar_senha("SenhaNumeros!"))
-
-    def test_senha_sem_caracteresp(self):
-        self.assertFalse(validar_senha("SenhaTop1"))
-
-    def test_senha_com_espaco(self):
-        self.assertFalse(validar_senha("Senha 123!"))
-
-    def test_senha_valida(self):
-        self.assertFalse(validar_senha("Senha123!"))
-if __name__ == "__main__":
-    unittest.main()
+def test_letra_maiuscula():
+    assert validacao_senha("asdadasd09") is False
